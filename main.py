275db7bf141b2cce7e3f8c4441d4dacb7e6de68a
Lexi115 <shinokataku1001@gmail.com>
2024-12-27 21:10:31 +0100

    fifth test (bernoulli) - better

--- main.py
+++ main.py
@@ -9,30 +9,21 @@ from sklearn.naive_bayes import BernoulliNB
 import utils
 
 
-def classify_hit(row):
-    if row['user_reviews'] >= 1200:
-        return 1
-    else:
-        return 0
-
 
 # Importa dataset dal file CSV e convertilo in un DataFrame
 df = pd.read_csv('./resources/games.csv')
 
-utils.scale(df, 'positive_ratio', 'positive_ratio_scaled')
-df['hit'] = df.apply(classify_hit, axis=1)
+df['user_reviews_bin'] = df['user_reviews'].apply(lambda x: 1 if x > 200 else 0)
+df['price_original_bin'] = df['price_original'].apply(lambda x: 1 if x >= 19.99 else 0)
+df['is_multiplatform'] = df[['win', 'mac', 'linux']].sum(axis=1).apply(lambda x: 1 if x > 1 else 0)
+df['before_2020'] = df['date_release'].apply(lambda x: 1 if datetime.strptime(x, '%Y-%m-%d').year < 2020 else 0)
+df['is_discounted'] = df['discount'].apply(lambda x: 1 if x > 0 else 0)
 
-df['price_final_bin'] = df['price_final'].apply(lambda x: 1 if x > 0 else 0)
-df['win_bin'] = df['win'].apply(lambda x: 1 if x == True else 0)
-df['mac_bin'] = df['mac'].apply(lambda x: 1 if x == True else 0)
-df['linux_bin'] = df['linux'].apply(lambda x: 1 if x == True else 0)
-df['discount_bin'] = df['discount'].apply(lambda x: 1 if x > 0 else 0)
+# target
+df['liked'] = df['positive_ratio'].apply(lambda x: 1 if x >= 70 else 0)
 
-
-print(df['hit'].value_counts())
-
-X = df[['price_final_bin', 'win_bin', 'mac_bin', 'linux_bin', 'discount_bin', 'positive_ratio_scaled']]
-y = df['hit']
+X = df[['user_reviews_bin', 'price_original_bin', 'is_multiplatform', 'before_2020', 'is_discounted']]
+y = df['liked']
 
 # Dividi in dati di train e test (Pareto 80/20)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
@@ -41,7 +32,7 @@ smote = SMOTE(random_state = 42)
 X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
 
 # Addestra il modello classificatore
-model = BernoulliNB(alpha = 0.5, fit_prior = True)
+model = BernoulliNB(alpha = 1, fit_prior = True)
 model.fit(X_train_resampled, y_train_resampled)
 
 # Fai predizioni
