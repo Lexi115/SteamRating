2ee26b72bd06cc33e459ba78104c35dd5e34f89c
Lexi115 <shinokataku1001@gmail.com>
2025-01-06 17:02:21 +0100

    16th test (bernoulli) - 76.2% - added k fold validation

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ y = df['liked']
 
 print("Distribution", y.value_counts())
 
+# k fold cross validation
+k = 5
+smoothing_factor_option = [1, 2, 3, 4, 5, 6]
+fit_prior_option = [True, False]
+auc_record = utils.get_auc_record(X, y, smoothing_factor_option, fit_prior_option, k)
+utils.print_auc_record(auc_record, k)
+
 # Dividi in dati di train e test (Pareto 80/20)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
@@ -28,13 +35,14 @@ print("Resampled y Train: ", y_train_resampled.shape)
 print("Resampled Distribution", y_train_resampled.value_counts())
 
 # Addestra il modello classificatore
-model = BernoulliNB(alpha=1.0, fit_prior=True)
+model = BernoulliNB(alpha=2.0, fit_prior=True)
 
 # Addestra il modello sui dati di training
 model.fit(X_train_resampled, y_train_resampled)
 
 # Fai predizioni
 y_pred = model.predict(X_test)
+y_prob = model.predict_proba(X_test)[:, 1]
 
 # Valuta modello usando le metriche di valutazione
-utils.print_metrics(y_test, y_pred)
+utils.print_metrics(y_test, y_pred, y_prob)
--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.over_sampling import SMOTE
 
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.naive_bayes import BernoulliNB
 from sklearn.preprocessing import MinMaxScaler
 
 
-def print_metrics(_test, _pred):
+def print_metrics(_test, _pred, _prob):
     print(f'Confusion Matrix:')
     print(confusion_matrix(_test, _pred, labels = [0, 1]))
 
@@ -21,6 +23,8 @@ def print_metrics(_test, _pred):
     f1 = f1_score(_test, _pred, pos_label = 1)
     print(f'F1 Score: {f1 * 100:.1f}%')
 
+    print('AUC with best model:', roc_auc_score(_test, _prob))
+
 
 def scale(_dataframe, _row, _new_row):
     scaler = MinMaxScaler()
@@ -35,3 +39,32 @@ def undersample(_x_train, _y_train, _strategy = 1):
 def oversample(_x_train, _y_train):
     overSampler = SMOTE()
     return overSampler.fit_resample(_x_train, _y_train)
+
+
+def get_auc_record(_x, _y, smoothing_factor_option, fit_prior_option, k):
+    auc_record = {}
+    k_fold = StratifiedKFold(n_splits=k, shuffle=True, random_state=42)
+
+    for train_indices, test_indices in k_fold.split(_x, _y):
+        X_train, X_test = _x.iloc[train_indices], _x.iloc[test_indices]
+        Y_train, Y_test = _y.iloc[train_indices], _y.iloc[test_indices]
+
+        for alpha in smoothing_factor_option:
+            if alpha not in auc_record:
+                auc_record[alpha] = {}
+            for fit_prior in fit_prior_option:
+                clf = BernoulliNB(alpha=alpha, fit_prior=fit_prior)
+                clf.fit(X_train, Y_train)
+                prediction_prob = clf.predict_proba(X_test)
+                pos_prob = prediction_prob[:, 1]
+                auc = roc_auc_score(Y_test, pos_prob)
+                auc_record[alpha][fit_prior] = auc + auc_record[alpha].get(fit_prior, 0.0)
+
+    return auc_record
+
+
+def print_auc_record(auc_record, k):
+    print('smoothing    fit prior   auc')
+    for smoothing, smoothing_record in auc_record.items():
+        for fit_prior, auc in smoothing_record.items():
+            print(f'{smoothing}            {fit_prior}       {auc/k:.5f}')
