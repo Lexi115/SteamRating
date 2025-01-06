c292d042501a1bbde645f59e3fe0097a16dfd7e2
Lexi115 <shinokataku1001@gmail.com>
2025-01-06 16:02:52 +0100

    15th test (bernoulli) - 76.2%

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-from datetime import datetime
-
-import pandas as pd
-from imblearn.under_sampling import RandomUnderSampler
-
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import BernoulliNB
 
+import helper
 import utils
 
 # Importa dataset dal file CSV e convertilo in un DataFrame
-df = pd.read_csv('./resources/games.csv')
+df = helper.get_dataframe_bernoulli()
 
-df['user_reviews_bin'] = df['user_reviews'].apply(lambda x: 1 if x >= 100 else 0)
-df['price_original_bin'] = df['price_original'].apply(lambda x: 1 if x >= 9.99 else 0)
-df['is_multiplatform'] = df[['win', 'mac', 'linux']].sum(axis=1).apply(lambda x: 1 if x > 1 else 0)
-df['before_2020'] = df['date_release'].apply(lambda x: 1 if datetime.strptime(x, '%Y-%m-%d').year < 2020 else 0)
-
-# target
-df['liked'] = df['positive_ratio'].apply(lambda x: 1 if x >= 60 else 0)
-
+# Selezione delle features e del target
 X = df[['user_reviews_bin', 'price_original_bin', 'is_multiplatform', 'before_2020']]
 y = df['liked']
 
-print("Distribution", df['liked'].value_counts())
+print("Distribution", y.value_counts())
 
 # Dividi in dati di train e test (Pareto 80/20)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
@@ -36,7 +25,7 @@ X_train_resampled, y_train_resampled = utils.undersample(X_train, y_train, 0.63)
 
 print("Resampled X Train: ", X_train_resampled.shape)
 print("Resampled y Train: ", y_train_resampled.shape)
-print("Distribution", y_train_resampled.value_counts())
+print("Resampled Distribution", y_train_resampled.value_counts())
 
 # Addestra il modello classificatore
 model = BernoulliNB(alpha=1.0, fit_prior=True)
