3390cb53694974bae635a5a1811e74167e960eab
Lexi115 <shinokataku1001@gmail.com>
2025-01-06 13:08:18 +0100

    twelfth test (bernoulli) - 76.2%

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import utils
 df = pd.read_csv('./resources/games.csv')
 
 df['user_reviews_bin'] = df['user_reviews'].apply(lambda x: 1 if x >= 100 else 0)
-df['price_original_bin'] = df['price_original'].apply(lambda x: 1 if x >= 69.99 else 0)
+df['price_original_bin'] = df['price_original'].apply(lambda x: 1 if x >= 9.99 else 0)
 df['is_multiplatform'] = df[['win', 'mac', 'linux']].sum(axis=1).apply(lambda x: 1 if x > 1 else 0)
 df['before_2020'] = df['date_release'].apply(lambda x: 1 if datetime.strptime(x, '%Y-%m-%d').year < 2020 else 0)
 
